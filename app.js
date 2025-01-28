9389bf260e4bda995387f640505d3675298a9954
pipe-ln <fel.lizama@gmail.com>
2025-01-28 18:56:07 -0300

    app,js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+let lista = document.getElementById ("listaAmigos"); // Lista de amigos ingresados
+// agregar amigo
+function agregarAmigo (){
+    let nombre = document.getElementById ("amigo").value 
+}
